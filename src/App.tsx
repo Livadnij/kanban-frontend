10a34cab57aafc87528eda344ef9c6bb9e391ac8
Antony Livadnyi <Livadnij.AV@gmail.com>
2024-05-22 15:02:54 +0300

    dndProvider for touch devices

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Box } from "@mui/joy";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 import { Header } from "./components/header/header";
 import { KanbanBoard } from "./components/kanbanBoard/kanbanBoard";
 
 function App() {
+  const isMobile =
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    );
+  const Backend = isMobile ? TouchBackend : HTML5Backend;
+
   return (
-    <DndProvider backend={HTML5Backend}>
+    <DndProvider backend={Backend}>
       <Box sx={{ margin: 2, marginLeft: "20px", marginRight: "20px" }}>
         <Header />
         <KanbanBoard />
